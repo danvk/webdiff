2a27dca8173408e78b536a3e955f736b7b696e15
Dan Vanderkam <danvdk@gmail.com>
2014-07-01 11:53:25 -0400

    bump version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='webdiff',
-      version='0.2',
+      version='0.2.1',
       description='Two-column web-based git difftool',
       author='Dan Vanderkam',
       author_email='danvdk@gmail.com',
