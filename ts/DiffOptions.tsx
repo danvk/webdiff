384993ee51fc16ee613f8a0f4536ad08a6dd37b8
Dan Vanderkam <danvdk@gmail.com>
2022-09-11 19:54:53 -0400

    wire up context + diff algorithm

--- ts/DiffOptions.tsx
+++ ts/DiffOptions.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactText } from 'react';
 
 import {DiffOptions} from './CodeDiff';
 
@@ -13,11 +13,28 @@ export function DiffOptionsControl(props: Props) {
   const toggleIgnoreAllSpace = () => {
     setOptions({...options, ignoreAllSpace: !options.ignoreAllSpace})
   };
+  const setUnifiedContext: React.ChangeEventHandler<HTMLInputElement> = e => {
+    setOptions({...options, unified: e.currentTarget.valueAsNumber});
+  };
+  const changeDiffAlgorithm: React.ChangeEventHandler<HTMLSelectElement> = e => {
+    setOptions({...options, diffAlgorithm: e.currentTarget.value as DiffOptions['diffAlgorithm']});
+  };
 
   return (
     <>
       <input type="checkbox" checked={!!options.ignoreAllSpace} id="ignore-all-space" onChange={toggleIgnoreAllSpace} />
       {' '}<label htmlFor='ignore-all-space'>Ignore All Space (<code>git diff -w</code>)</label>
+      <br/>
+      Context:{' '}
+      <input type="number" min={1} max={100} value={options.unified ?? 8} onChange={setUnifiedContext} />
+      <br/>
+      Diff Algorithm:{' '}
+      <select value={options.diffAlgorithm ?? 'myers'} onChange={changeDiffAlgorithm}>
+        <option value="myers">Myers (Default)</option>
+        <option value="patience">Patience</option>
+        <option value="minimal">Minimal</option>
+        <option value="histogram">Histogram</option>
+      </select>
     </>
   );
 }
