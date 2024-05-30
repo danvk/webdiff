b54665ce947f8f65b67cba617435a1473e5451a8
Dan Vanderkam <danvdk@gmail.com>
2024-05-30 17:41:52 -0400

    set up test script with nodemon

--- test.sh
+++ test.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-export PYTHONPATH=.
-./webdiff/app.py $*
+export WEBDIFF_PORT=6001
+export WEBDIFF_NO_OPEN=true
+
+sleep 1 && open 'http://localhost:6001' &
+
+npx nodemon \
+    -w webdiff \
+    --ext py,html,js \
+    -x python \
+    webdiff/app.py $*
